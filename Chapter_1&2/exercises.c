0d3d51f4efeac9fe82d77d23eaac071ab6f80945
David Latorre <david@latorredev.com>
2019-01-27 21:41:53 -0500

    Add chapter1&2 folder with exercises

---
+++ Chapter_1&2/exercises.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf ("In C, lowercase letters are significant.\n main() is where program execution begins.\n");
+    printf ("Opening and closing braces enclose program statements in a routine.\n All program statements must be terminated by a semicolon.\n");
+}
---
+++ Chapter_1&2/substract.c
@@ -0,0 +1,20 @@
+/* 
+    Program for substract 
+    two numbers
+*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    //Declare variables
+    int value1, value2, sub;
+
+    // Assing values and compute
+    value1 = 87;
+    value2 = 15;
+    sub = value1 - value2;
+
+    //Display result
+    printf ("The substraction of %i and %i is %i\n", value1, value2, sub);
+}
